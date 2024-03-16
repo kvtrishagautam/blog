6090b0e916ec33d638faed6e6e26908fbc1a950d
Trisha <kvtrishagautam@gamil.com>
2024-03-16 17:41:37 +0530

    Home:FeaturedPosts done

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,14 @@
 import Image from 'next/image'
 import{allBlogs} from "contentlayer/generated"
 import HomeCoverSection from '../components/Home/HomeCoverSection';
+import FeaturedPosts from '../components/Home/FeaturedPosts'
+
 export default function Home() {
   console.log(allBlogs);
   return (
     <main className="flex flex-col items-center justify-center">
       <HomeCoverSection blogs={allBlogs} />
+      <FeaturedPosts blogs={allBlogs} />
     </main>
   )
 }
