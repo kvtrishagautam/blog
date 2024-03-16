4fe70bfcb823b92bd374e614889d73b02b5a6b2c
Trisha <kvtrishagautam@gamil.com>
2024-03-16 14:29:22 +0530

    Initial commit

---
+++ src/components/Header/Header/index.js
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import Logo from "./Logo";
+import { DribbbleIcon, GithubIcon, LinkedinIcon, SunIcon, TwitterIcon } from "@/src/components/Header/Icons";
+
+const Header = () => {
+  return (
+    <header className="w-full p-4 px-10 flex items-center justify-between">
+        <Logo/>
+        <nav className="w-max py-3 px-8 border border-solid border-dark rounded-full font-medium capitalize flex items-center fixed top-6 right-1/2 translate-x-1/2 bg-light/80 backdrop-blur-sm">
+            <Link href="/" className="mr-2">Home</Link>
+            <Link href="/about"className="mx-2">About</Link>
+            <Link href="/contact" className="mr-2">Contact</Link>
+            <button>
+                <SunIcon/>
+            </button>
+        </nav>
+        <div className="flex "> 
+            <a href="" className="inline block w-9 h-9 mr-4"><LinkedinIcon className="hover:scale-125 transition-all ease duration-200" /></a>
+            <a href="" className="inline block w-9 h-9 mr-4"><TwitterIcon  className="hover:scale-125 transition-all ease duration-200"/></a>
+            <a href="" className="inline block w-9 h-9 mr-4"><GithubIcon   className="hover:scale-125 transition-all ease duration-200"/> </a>
+            <a href="" className="inline block w-9 h-9 mr-4"><DribbbleIcon className="hover:scale-125 transition-all ease duration-200"/></a>
+        </div>
+    </header>
+  )
+}
+
+export default Header;
